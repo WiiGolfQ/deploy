86e090d35b621277bb7fa8d3422a09870bf2db65
vncz14 <57498723+vncz14@users.noreply.github.com>
2024-03-08 01:26:27 -0500

    edit secrets script

--- secrets.sh
+++ secrets.sh
@@ -2,7 +2,12 @@ secrets=("postgres_password" "discord_bot_token" "django_secret")
 
 for secret in "${secrets[@]}"; do
     echo "${secret}:"
-    read value
+    read input
+    if [[ $input == \$* ]]; then
+        value=$(eval "${input:1}")
+    else
+        value=$input
+    fi
     if docker secret inspect $secret > /dev/null 2>&1; then
         docker secret rm $secret
     fi
