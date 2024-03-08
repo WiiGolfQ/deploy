541a5a137a4b890b4bb7908fb931ba3ba792e3ed
vncz14 <57498723+vncz14@users.noreply.github.com>
2024-03-07 19:56:46 -0500

    Update start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+git clone "https://github.com/WiiGolfQ/backend/" "backend"
+git clone "https://github.com/WiiGolfQ/discord-bot/" "discord-bot"
+git clone "https://github.com/WiiGolfQ/website/" "website"
+
+docker-compose up -d
