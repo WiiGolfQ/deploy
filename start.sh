838df049c985bd576398ac1fa794dbc38aa9750f
vncz14 <57498723+vncz14@users.noreply.github.com>
2024-03-07 20:08:00 -0500

    update script

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
-git clone "https://github.com/WiiGolfQ/backend/" "backend"
-git clone "https://github.com/WiiGolfQ/discord-bot/" "discord-bot"
-git clone "https://github.com/WiiGolfQ/website/" "website"
+set -e
 
-docker-compose up -d
+declare -A repos=(
+    ["backend"]="https://github.com/WiiGolfQ/backend/"
+    ["discord-bot"]="https://github.com/WiiGolfQ/discord-bot/"
+    ["website"]="https://github.com/WiiGolfQ/website/"
+)
+
+for dir in "${!repos[@]}"; do
+    if [ ! -d "$dir" ]; then
+        git clone "${repos[$dir]}" "$dir"
+    else
+        pushd "$dir"
+        git pull
+        popd
+    fi
+done
